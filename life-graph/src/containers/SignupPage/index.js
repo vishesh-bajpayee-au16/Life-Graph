b3344f2482b14bb5345f5b20a7f7617ef2cd3d6d
vishesh-bajpayee-au16 <vishu22101999@gmail.com>
2021-07-26 14:56:43 +0530

    [SIGNUP PAGE] added styles to signup page

--- life-graph/src/containers/SignupPage/index.js
+++ life-graph/src/containers/SignupPage/index.js
@@ -1,7 +1,39 @@
 import React from "react";
-
+import Button from "../../components/Buttons";
+import FormInput from "../../components/FormInput";
+import "./index.scss";
 const SignupPage = () => {
-  return <div>Signup</div>;
+  return (
+    <div className="login-fragment">
+      <div className="login-container">
+        <h1>Create your acount </h1>
+        <FormInput type="email" id="email" label="Full Name" width="40vh" />
+        <FormInput type="Email" id="email" label="User Name" width="40vh" />
+        <FormInput type="Email" id="email" label="Email" width="40vh" />
+        <FormInput
+          type="password"
+          id="password"
+          label="Password"
+          width="40vh"
+        />
+
+        <Button
+          className="login-btn"
+          text="Login"
+          color="white"
+          backgroundColor="crimson"
+          link="/user/dashboard"
+        />
+        <Button
+          className="login-btn"
+          text="Login with google"
+          color="white"
+          backgroundColor="grey"
+          link="/user/dashboard"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default SignupPage;
